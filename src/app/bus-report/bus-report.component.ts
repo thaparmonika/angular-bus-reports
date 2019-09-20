ba0795aa2b83a62967be5fe5b0539c838a0e16c3
thaparmonika <thaparmonika@yahoo.com.au>
2019-09-20 15:07:13 +0530

    feedback submission confirmation

--- src/app/bus-report/bus-report.component.ts
+++ src/app/bus-report/bus-report.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 import { BusReport } from './bus-report';
 
@@ -10,8 +11,12 @@ import { BusReport } from './bus-report';
 export class BusReportComponent implements OnInit {
   @Input() BusReport: BusReport;
   displayedColumns: string[] = ['busId', 'routeVariant', 'deviationFromTimetable'];
-  constructor() { }
-
+  constructor(private _snackBar: MatSnackBar) {}
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
   ngOnInit() {
   }
 
