a56ba6b852e49c4118b259cd6ccfcbc0999a0305
thaparmonika <thaparmonika@yahoo.com.au>
2019-09-20 10:37:22 +0530

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BusReportComponent } from './bus-report/bus-report.component';
+import { BusListComponent } from './bus-list/bus-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, BusReportComponent, BusListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/bus-report/bus-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bus-report',
+  templateUrl: './bus-report.component.html',
+  styleUrls: ['./bus-report.component.css']
+})
+export class BusReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
